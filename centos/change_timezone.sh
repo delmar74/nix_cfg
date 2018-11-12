5ab523e7a1d4640f3ae234d405510078a3420555
oleg <oleg@localhost.localdomain>
2018-11-12 22:19:33 +0300

    first scripts for centos

---
+++ centos/change_timezone.sh
@@ -0,0 +1,17 @@
+
+# View Current Local Time 
+timedatectl
+
+
+# List All Available Time Zone
+timedatectl list-timezones
+
+# Search my Time Zone
+#timedatectl list-timezones | grep 'Moscow'
+
+# Set my Time Zone
+#timedatectl set-timezone Europe/Moscow
+
+# View New Config
+#timedatectl
+
